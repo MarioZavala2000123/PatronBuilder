5abc88cec7da2587ccd0d289788223b0ebe120f4
MarioZavala2000123 <mrzavalat@alumno.uspg.edu.gt>
2021-09-30 10:05:32 -0600

    primer commir (vacio)

---
+++ src/main/java/edu/uspg/PatronBuilderApplication.java
@@ -0,0 +1,13 @@
+package edu.uspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uspg/PatronBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uspg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
